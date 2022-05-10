cca4fe8f7a72b441c8745cbf0a0f89e6bd18f293
code1218 <skjil1218@gmail.com>
2022-05-10 19:17:48 +0900

    usermanagementprogram start

---
+++ src/UserManagementProgram/DB/DBConnection.py
@@ -0,0 +1,22 @@
+import pymysql
+
+class DBConnectionPy:
+
+    @staticmethod
+    def getConnection():
+        db = "python_junil"
+
+        print(f"[{db}] 데이터 베이스를 연결합니다.")
+        con = pymysql.connect(
+            host = "127.0.0.1",
+            port = 8001,
+            user = "root",
+            password = "toor",
+            database = db,
+            charset = "utf8"
+        )
+        return con
+
+    @staticmethod
+    def freeConnection(connect):
+        connect.close()
